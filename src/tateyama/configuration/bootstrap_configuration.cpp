4d84f782f65e953837e4450a9acec72291fd4594
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2024-10-25 00:30:32 +0900

    add config. param. dev_direct_commit_callback

--- src/tateyama/configuration/bootstrap_configuration.cpp
+++ src/tateyama/configuration/bootstrap_configuration.cpp
@@ -48,6 +48,7 @@ static constexpr std::string_view default_configuration {  // NOLINT
         "scan_yield_interval=1\n"
         "dev_rtx_parallel_scan=false\n"
         "dev_thousandths_ratio_check_local_first=100\n"
+        "dev_direct_commit_callback=false\n"
 
     "[ipc_endpoint]\n"
         "database_name=tsurugi\n"
