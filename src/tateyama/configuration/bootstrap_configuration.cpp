8a8a1029eeb5b77d30506f8cb0b726c1e7d10db8
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2025-03-08 11:04:59 +0900

    update config. param. to enable blob cast by default

--- src/tateyama/configuration/bootstrap_configuration.cpp
+++ src/tateyama/configuration/bootstrap_configuration.cpp
@@ -54,7 +54,7 @@ static constexpr std::string_view default_configuration {  // NOLINT
         "dev_inplace_dag_schedule=true\n"
         "enable_join_scan=true\n"
         "dev_rtx_key_distribution=uniform\n"
-        "dev_enable_blob_cast=false\n"
+        "dev_enable_blob_cast=true\n"
         "max_result_set_writers=64\n"
 
     "[ipc_endpoint]\n"
