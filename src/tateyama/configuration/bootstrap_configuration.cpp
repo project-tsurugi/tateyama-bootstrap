3f54b9d62a341ade7dc8c0e901bcc292d46754eb
Nobuhiro Ban <ban@nautilus-technologies.com>
2025-07-22 15:27:03 +0900

    add shirakami parallel recovery thread num setting for project-tsurugi/tsurugi-issues#1239

--- src/tateyama/configuration/bootstrap_configuration.cpp
+++ src/tateyama/configuration/bootstrap_configuration.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2023 Project Tsurugi.
+ * Copyright 2018-2025 Project Tsurugi.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -91,6 +91,7 @@ static constexpr std::string_view default_configuration {  // NOLINT
         "epoch_duration=3000\n"
         "waiting_resolver_threads=2\n"
         "max_concurrent_transactions=\n"
+        "index_restore_threads=4\n"
 
     "[system]\n"
         "pid_directory=/var/lock\n"
