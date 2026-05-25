3228c95101f58f0cb672ee45a94c4c5ddc27180b
t-horikawa <horikawa@nautilus-technologies.com>
2026-05-25 12:57:49 +0900

    update test/tateyama/test_utils/status_mock.h (test code change only)

--- test/tateyama/test_utils/status_mock.h
+++ test/tateyama/test_utils/status_mock.h
@@ -26,6 +26,38 @@
 
 namespace tateyama::test_utils {
 
+class resource_status_adapter {
+public:
+    resource_status_adapter(const std::string& file_name, std::size_t size)
+        : mem_(std::make_unique<boost::interprocess::managed_shared_memory>(boost::interprocess::create_only, file_name.c_str()\
+, size)),
+          file_name_(file_name),
+          resource_status_memory_(mem_->construct<tateyama::status_info::resource_status_memory::resource_status_memory::resource_status>(std::string(tateyama::status_info::resource_status_memory::area_name).c_str())(mem_->get_segment_manager())) {
+    }
+    ~resource_status_adapter() {
+        try {
+            mem_->destroy<tateyama::status_info::resource_status_memory::resource_status_memory::resource_status>(std::string(tateyama::status_info::resource_status_memory::resource_status_memory::area_name).c_str());
+            boost::interprocess::shared_memory_object::remove(file_name_.c_str());
+        } catch (std::exception& e) {
+            LOG(WARNING) << e.what();
+        }
+    }
+
+    resource_status_adapter(resource_status_adapter const& other) = delete;
+    resource_status_adapter& operator=(resource_status_adapter const& other) = delete;
+    resource_status_adapter(resource_status_adapter&& other) noexcept = delete;
+    resource_status_adapter& operator=(resource_status_adapter&& other) noexcept = delete;
+
+    tateyama::status_info::resource_status_memory* resource_status_memory_address() {
+        return &resource_status_memory_;
+    }
+
+private:
+    std::unique_ptr<boost::interprocess::managed_shared_memory> mem_;
+    std::string file_name_;
+    tateyama::status_info::resource_status_memory resource_status_memory_;
+};
+
 class status_mock {
     static constexpr std::string_view file_prefix = "tsurugidb-";
     static constexpr std::string_view mutex_file_prefix = "tsurugi-";
@@ -38,8 +70,8 @@ public:
         status_file_name_ += digest;
         status_file_name_ += ".stat";
         try {
-            segment_ = std::make_unique<boost::interprocess::managed_shared_memory>(boost::interprocess::create_only, status_file_name_.c_str(), shm_size);
-            resource_status_memory_ = std::make_unique<tateyama::status_info::resource_status_memory>(*segment_);
+            resource_status_adapter_ = std::make_unique<resource_status_adapter>(status_file_name_, shm_size);
+            resource_status_memory_=resource_status_adapter_->resource_status_memory_address();
             resource_status_memory_->set_pid();
             resource_status_memory_->set_database_name(name);
             std::string mutex_file_name{directory};
@@ -61,8 +93,8 @@ public:
 
 private:
     std::string status_file_name_{};
-    std::unique_ptr<boost::interprocess::managed_shared_memory> segment_{};
-    std::unique_ptr<tateyama::status_info::resource_status_memory> resource_status_memory_{};
+    std::unique_ptr<resource_status_adapter> resource_status_adapter_{};
+    tateyama::status_info::resource_status_memory* resource_status_memory_{};
 };
 
 }  // namespace tateyama::testing
